2e5c984516ab2c3a52c74a47e64a6b10a8a69719
Ace Nanter <ace.nanter@hotmail.fr>
2023-03-03 10:43:29 +0100

    Initialized monorepo

--- src/modules/index.ts
+++
@@ -1,7 +0,0 @@
-export { AdministrationModule as AdministrationModule } from './administration/administration.module';
-export { GuildMessagesModule as GuildMessagesModule } from './guild-messages/guild-messages.module';
-export { GamesRolesModule as GamesRolesModule } from './games-roles/games-roles.module';
-export { NewRoleModule as NewRoleModule } from './new-role.module';
-export { MusicModule as MusicModule } from './music/music.module';
-export { SpyModule as SpyModule } from './spy.module';
-export { MinecraftModule as MinecraftModule } from './minecraft/minecraft.module';
